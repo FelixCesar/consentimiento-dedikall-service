3aad59192a6554d95466752e1604040a7dbef2bb
felix <felixcesar2078@gmail.com>
2026-03-06 10:50:02 -0500

    Eliminar la importación de provideClientHydration y su uso en la configuración de la aplicación

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http'; // <-- IMPORTAR ESTO
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideClientHydration(withEventReplay()),
     provideHttpClient()
   ]
 };
